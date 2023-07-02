299c84c920175a2d0782c1a7f5024511df6a6464
RazziAftab <razziaftab@gmail.com>
2023-07-02 16:56:44 +0200

    Add the fillable in Item model

--- app/Item.php
+++ app/Item.php
@@ -9,4 +9,8 @@ class Item extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['entity_id', 'category_name', 'sku', 'name', 'description', 'short_description', 'price',
+        'link', 'image', 'brand', 'rating', 'caffeine_type', 'count', 'flavored', 'seasonal', 'in_stock', 'facebook',
+        'is_kcup'];
+    
 }
